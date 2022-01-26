0657f2646f165ede28402411d000d252bed745ca
saba8814 <ejub1946@outlook.com>
2022-01-26 19:58:01 +0100

    filter-applied

--- main.py
+++ main.py
@@ -17,7 +17,12 @@ while True:
         break
     if c == 32:
         img_name = datetime.now().strftime("%d-%m-%Y-%H%M%S")+".png"
-        cv2.imwrite(img_name, frame)
+        grey_img=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        invert_img=cv2.bitwise_not(grey_img)
+        blur_img=cv2.GaussianBlur(invert_img, (105,105),0)
+        invblur_img=cv2.bitwise_not(blur_img)
+        sketch_img=cv2.divide(grey_img,invblur_img, scale=200)
+        cv2.imwrite(img_name, sketch_img)
         print("{} written!".format(img_name))
 
 camera.release()
